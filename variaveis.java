d1754cfbd32f17a368e54e229850191958d7c0ce
FelipeGabrielRF <frfelipe10@gmail.com>
2024-07-04 15:20:24 -0300

    Add files via upload

---
+++ variaveis.java
@@ -0,0 +1,17 @@
+public class variaveis {
+    public static void main(String[] args) throws Exception {
+        int varInt = 2;
+
+        Integer varInteger = 3;
+
+        String varString = "texto";
+
+        boolean varBoolean = true;
+
+        byte varByte = 1;
+
+        char varChar = 'r';
+
+        float varFloat = 19.99f;
+    }
+}
