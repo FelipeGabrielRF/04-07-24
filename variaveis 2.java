588998b848841b631a05180ab097a2e20fd260db
FelipeGabrielRF <frfelipe10@gmail.com>
2024-07-04 15:22:53 -0300

    Add files via upload

---
+++ variaveis 2.java
@@ -0,0 +1,29 @@
+public class Variaveis {
+    public static void main(String[] args) throws Exception {
+        int varInt = 2; // declaração da variável varInt, do tipo int, com valor recebido 2
+        System.out.println("Variável do tipo int (inteiro): " + varInt);
+
+        Integer varInteger = 3; // declaração da variavel varInteger, do tipo inteiro, com valor recebido 3
+        System.out.println("Variável do tipo Integer (inteiro): " + varInteger);
+
+        String varString = "texto"; // declaração da variavel varString, do tipo texto
+        System.out.println("Variável do tipo String (texto): " + varString);
+
+        boolean varBoolean = true; // declaração da variavel varBoolean,do tipo boolean, 0 ou 1 (verdadeiro ou
+                                   // falso)
+        System.out.println(varBoolean);
+
+        byte varByte = 1; // declaração da variavel varByte, do tipo byte, com valor recebido 1
+        System.out.println(varByte);
+
+        char varChar = 'r'; // declaração da variavel varChar, do tipo char, aceita apenas 1 caracter
+        System.out.println(varChar);
+
+        float varFloat = 19.99f; // declaração da variavel varFloat, do tipo float, com valor recebido 19.99f
+        System.out.println(varFloat);
+
+        double varDouble = 0.263568954; // declaração da vairavel varDouble, do tipo double, com valor recebido
+                                        // 0.263568954
+        System.out.println(varDouble);
+    }
+}
